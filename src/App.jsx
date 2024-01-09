6e560d102b31d0f704ccc69556a4ff6ff301b864
martyasbutkus <noobtuberis1990@gmail.com>
2024-01-09 19:37:25 +0200

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Products from './pages/products';
-import Form from './pages/form';
-import Nav from './pages/nav';
+import Products from './pages/Products';
+import Form from './pages/Form';
+import Nav from './pages/Nav';
 
 function App() {
   return (
